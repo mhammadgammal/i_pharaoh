a02a2a107738d13896d663dbf19a9b4e7c3c32c9
mhammadgammal <mhammadgamal17@gmail.com>
2024-08-10 11:56:21 +0300

    add image asset

---
+++ lib/core/theme/app_images.dart
@@ -0,0 +1,15 @@
+abstract class AppImages {
+  // LOGO
+  static const String appLogo = 'assets/images/logo/app_icon.svg';
+  static const String appLogoNoBackground =
+      'assets/images/logo/app_icon_no_background.svg';
+
+  // ON BOARDING
+  static const onBoarding1 = 'assets/images/on_boarding/on_boarding_1.png';
+  static const onBoarding2 = 'assets/images/on_boarding/on_boarding_2.png';
+  static const onBoarding3 = 'assets/images/on_boarding/on_boarding_3.png';
+
+  // LOGIN BACKGROUND
+  static const loginBackground = 'assets/images/login/login_background.png';
+// REGISTER BACKGROUND
+}
