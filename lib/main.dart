4100ea6a01d7c2f4bb69175cf7aefa9de0f431c3
Mohamed Gammal <mhammadgamal17@gmail.com>
2024-08-12 17:55:11 +0300

    register firebase auth instance and needed auth instances

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:i_pharaoh/core/di/di.dart';
+import 'package:i_pharaoh/core/observer/bloc_observer.dart';
 
 import 'i_pharo.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   init();
+  Bloc.observer = IPharoeBlocObserver();
   runApp(const IPharo());
 }
