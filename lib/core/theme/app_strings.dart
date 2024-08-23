d70c3801c8ef6d40271f59d9667446ca78255763
Mohamed Gammal <mhammadgamal17@gmail.com>
2024-08-23 18:51:05 +0300

    add login strings

--- lib/core/theme/app_strings.dart
+++ lib/core/theme/app_strings.dart
@@ -1,11 +1,16 @@
 abstract class AppStrings {
   //on boarding strings
-  static const String onBoardingText1 = 'Coming to visit one of the Egyptian Landmarks ?';
-  static const String onBoardingText2 = 'Need information about specific artifact ?';
-  static const String onBoardingText3 = 'Don’t worry all you need is to scan it with your phone';
+  static const String onBoardingText1 =
+      'Coming to visit one of the Egyptian Landmarks ?';
+  static const String onBoardingText2 =
+      'Need information about specific artifact ?';
+  static const String onBoardingText3 =
+      'Don’t worry all you need is to scan it with your phone';
 
   // Authentication strings
   static const String welcome = 'Welcome';
   static const String emailAddress = 'Email Address';
   static const String password = 'Password';
+  static const String signIn = 'Sign In';
+  static const String loginError = 'Error Credentials';
 }
