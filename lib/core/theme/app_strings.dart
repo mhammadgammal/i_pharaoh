4eadc210d26bd86082a08b77ec9ed9a3754e2d7d
Mohamed Gammal <mhammadgamal17@gmail.com>
2024-08-23 15:14:11 +0300

    add app strings file

---
+++ lib/core/theme/app_strings.dart
@@ -0,0 +1,11 @@
+abstract class AppStrings {
+  //on boarding strings
+  static const String onBoardingText1 = 'Coming to visit one of the Egyptian Landmarks ?';
+  static const String onBoardingText2 = 'Need information about specific artifact ?';
+  static const String onBoardingText3 = 'Don’t worry all you need is to scan it with your phone';
+
+  // Authentication strings
+  static const String welcome = 'Welcome';
+  static const String emailAddress = 'Email Address';
+  static const String password = 'Password';
+}
