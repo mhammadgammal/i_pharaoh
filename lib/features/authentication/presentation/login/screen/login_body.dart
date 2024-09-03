6469c380b048d7f01ef715bed142b55e8aba95da
Mohamed Gammal <mhammadgamal17@gmail.com>
2024-09-03 17:21:52 +0300

    add continue with google button and add navigation to register

--- lib/features/authentication/presentation/login/screen/login_body.dart
+++ lib/features/authentication/presentation/login/screen/login_body.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:i_pharaoh/core/router/app_navigator.dart';
 import 'package:i_pharaoh/core/theme/app_colors.dart';
 import 'package:i_pharaoh/core/theme/app_strings.dart';
 import 'package:i_pharaoh/core/widgets/default_form_field.dart';
 import 'package:i_pharaoh/features/authentication/presentation/login/cubit/login_cubit.dart';
 import 'package:i_pharaoh/features/authentication/presentation/widgets/authentication_button.dart';
+import 'package:i_pharaoh/features/authentication/presentation/widgets/continue_with_google.dart';
+
+import '../../widgets/custom_dashed_line.dart';
 
 class LoginBody extends StatelessWidget {
   const LoginBody({super.key});
@@ -74,6 +78,63 @@ class LoginBody extends StatelessWidget {
                     isLoading: state is LoginLoading,
                     title: AppStrings.signIn,
                     onPressed: () => cubit.signIn(),
+                  ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  Row(
+                    children: [
+                      const Expanded(
+                          child: Padding(
+                        padding: EdgeInsets.all(5.0),
+                        child: CustomDashedLine(
+                          color: Colors.blueGrey,
+                        ),
+                      )),
+                      Text(
+                        ' OR ',
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                              fontSize: 17.0,
+                            ),
+                      ),
+                      const Expanded(
+                          child: Padding(
+                        padding: EdgeInsets.all(5.0),
+                        child: CustomDashedLine(
+                          color: Colors.blueGrey,
+                        ),
+                      )),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 15.0,
+                  ),
+                  const ContinueWithGoogle(),
+                  const SizedBox(
+                    height: 15.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        AppStrings.dontHaveAcc,
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyMedium
+                            ?.copyWith(fontSize: 15.0),
+                      ),
+                      TextButton(
+                        onPressed: () =>
+                            AppNavigator.navigateAndReplaceToSignUp(context),
+                        child: Text(
+                          AppStrings.registerNow,
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyMedium
+                              ?.copyWith(fontSize: 15.0),
+                        ),
+                      )
+                    ],
                   )
                 ],
               ),
