3f70471b9caf5d190b0598bbbd5d816d81cf2be4
Mohamed Gammal <mhammadgamal17@gmail.com>
2024-09-03 17:24:29 +0300

    remove redundant bloc providing

--- lib/features/authentication/presentation/login/screen/login_screen.dart
+++ lib/features/authentication/presentation/login/screen/login_screen.dart
@@ -7,37 +7,34 @@ import 'package:i_pharaoh/core/widgets/error_dialogue.dart';
 import 'package:i_pharaoh/features/authentication/presentation/login/cubit/login_cubit.dart';
 import 'package:i_pharaoh/features/authentication/presentation/login/screen/login_body.dart';
 
-import '../../../../../core/di/di.dart';
+// import '../../../../../core/di/di.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => LoginCubit(sl.get()),
-      child: BlocListener<LoginCubit, LoginState>(
-        listener: (context, state) {
-          if (state is LoginSuccessState) {
-            AppNavigator.navigateToHome(context);
-          } else if (state is LoginFailureState) {
-            showDialog(
-                context: context,
-                builder: (context) =>
-                    const ErrorDialogue(errorMessage: AppStrings.loginError));
-          }
-        },
-        child: Scaffold(
-            body: Container(
-          alignment: Alignment.topCenter,
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage(AppImages.loginBackground),
-                fit: BoxFit.fitWidth),
-          ),
-          child: const LoginBody(),
-        )),
-      ),
+    return BlocListener<LoginCubit, LoginState>(
+      listener: (context, state) {
+        if (state is LoginSuccessState) {
+          AppNavigator.navigateToHome(context);
+        } else if (state is LoginFailureState) {
+          showDialog(
+              context: context,
+              builder: (context) =>
+                  const ErrorDialogue(errorMessage: AppStrings.loginError));
+        }
+      },
+      child: Scaffold(
+          body: Container(
+        alignment: Alignment.topCenter,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage(AppImages.loginBackground),
+              fit: BoxFit.fitWidth),
+        ),
+        child: const LoginBody(),
+      )),
     );
   }
 }
